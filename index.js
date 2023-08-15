d8ac34ded18f1680c2856b02404d42863e8cf853
k3v <25158905+k3v53@users.noreply.github.com>
2023-08-15 00:37:20 -0300

    Testing secrets

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log('Hello World from nodejs');
+require('dotenv').config();
+const { Client } = require('@notionhq/client');
+const { get } = require('https');
+const { JSDOM } = require('jsdom');
+const { Calibre } = require('node-calibre');
+const { NOTION_DATABASE_ID, NOTION_API_KEY } = process.env;
+const notion = new Client({ auth: NOTION_API_KEY });
+const calibre = new Calibre();
+
+async function listNotionBooks() {
+	try {
+		const response = await notion.databases.query({
+			database_id: NOTION_DATABASE_ID,
+			sorts: [
+				{
+					property: 'Última edición',
+					direction: 'ascending',
+				},
+			],
+			filter: {
+				and: [
+					{
+						property: 'procesing_status',
+						multi_select: { does_not_contain: 'Procesed Initial Upload' },
+					},
+				],
+			},
+		});
+		return response;
+	} catch (error) {
+		console.log(error);
+	}
+}
+async function main(params) {
+	let notionBooks = await listNotionBooks();
+	console.log(notionBooks);
+	const calibreResponse = await calibre.run(
+		'fetch-ebook-metadata -t "El alquimista"'
+	);
+	console.log(calibreResponse);
+}
+main();
